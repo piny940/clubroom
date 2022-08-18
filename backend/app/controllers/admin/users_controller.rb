ede358c33b1902ff392fce25e6b9f7af18730122
mikan <83708535+piny940@users.noreply.github.com>
2022-08-19 02:26:56 +0900

    ユーザー管理画面のレイアウトを修正

--- backend/app/controllers/admin/users_controller.rb
+++ backend/app/controllers/admin/users_controller.rb
@@ -23,27 +23,19 @@ class Admin::UsersController < ApplicationController
   def create
     @user = User.new(user_params)
 
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to user_url(@user), notice: "User was successfully created." }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    if @user.save
+      redirect_to admin_users_path, notice: "ユーザーを作成しました"
+    else
+      render :new, status: :unprocessable_entity
     end
   end
 
   # PATCH/PUT /admin/users/1 or /admin/users/1.json
   def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to user_url(@user), notice: "User was successfully updated." }
-        format.json { render :show, status: :ok, location: @user }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    if @user.update(user_params)
+      redirect_to admin_user_path(@user), notice: "Userが更新されました。"
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
@@ -51,10 +43,7 @@ class Admin::UsersController < ApplicationController
   def destroy
     @user.destroy
 
-    respond_to do |format|
-      format.html { redirect_to users_url, notice: "User was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    redirect_to admin_users_path
   end
 
   private
@@ -65,6 +54,6 @@ class Admin::UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.fetch(:user, {})
+      params.require(:user).permit(:email, :password, :password_confirmation, :kind)
     end
 end
--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -2,8 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
-         :confirmable
+         :recoverable, :rememberable, :validatable
   
   enum kind: { member: 0, admin: 1 }, _prefix: true
 end
