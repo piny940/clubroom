03776d3c1f8731091fa6c571f393d0671c60908a
mikan <83708535+piny940@users.noreply.github.com>
2022-08-27 13:20:10 +0900

    Layoutのテストを追加

---
+++ frontend/__tests__/components/Layout.test.tsx
@@ -0,0 +1,24 @@
+import { render, waitFor } from '@testing-library/react'
+import { Layout } from '../../components/Layout'
+import { TestID } from '../../resources/TestID'
+import { Test } from '../testHelpers/Test'
+
+jest.mock('../../containers/Navbar.tsx', () => ({
+  Navbar: () => {
+    return <div />
+  },
+}))
+
+describe('<Layout />', () => {
+  it('正常に描画される', async () => {
+    const { getByTestId } = render(
+      <Layout>
+        <Test />
+      </Layout>
+    )
+
+    await waitFor(() => {
+      expect(getByTestId(TestID.TEST)).toBeTruthy()
+    })
+  })
+})
