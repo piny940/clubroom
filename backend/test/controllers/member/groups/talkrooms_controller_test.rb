a36b9c4a9814d6f349838f35f0e2b5b27ed35558
mikan <83708535+piny940@users.noreply.github.com>
2022-08-28 13:41:15 +0900

    talkrooms_controllerのテストを修正

--- backend/test/controllers/member/groups/talkrooms_controller_test.rb
+++ backend/test/controllers/member/groups/talkrooms_controller_test.rb
@@ -1,28 +1,33 @@
 require 'test_helper'
 
 class Member::Groups::Talkrooms_controller < ActionDispatch::IntegrationTest
+  def setup
+    @user = User.create!(name: 'alice', email: 'alice@example.com', password: 'password', password_confirmation: 'password')
+    @group1 = Group.create!(name: 'group1')
+    @group2 = Group.create!(name: 'group2')
+    @group1.members << @user
+    @room1 = @group1.talkrooms.create!(name: 'room1')
+    @room2 = @group1.talkrooms.create!(name: 'room2')
+    @room1.members << @user
+  end
+
   test '指定されたグループに所属していない場合は400を返す' do
-    user = users('kaguya')
-    group = groups('shiroganeke')
-    sign_in user
+    sign_in @user
     
-    get "/member/groups/#{group.id}/talkrooms"
+    get "/member/groups/#{@group2.id}/talkrooms"
     assert_response 400
     json = JSON.parse(response.body)
     assert_equal json['message'], 'このグループには所属していません。'
   end
 
   test '指定されたグループのトークルームの中で、自身が入っているものだけを取得できる' do
-    user = users('kaguya')
-    group = groups('shinomiyake')
-    talk_room = talkrooms('shinomiyake-room1')
-    sign_in user
+    sign_in @user
 
-    get "/member/groups/#{group.id}/talkrooms"
+    get "/member/groups/#{@group1.id}/talkrooms"
     assert_response 200
     json = JSON.parse(response.body)
 
-    assert_equal json["data"]["talkrooms"][0]["name"], talk_room.name
+    assert_equal json["data"]["talkrooms"][0]["name"], @room1.name
     assert_equal json["data"]["talkrooms"].length, 1
   end
 end
