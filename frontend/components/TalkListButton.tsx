0bee5daf6bd60ef04d70da341cac67217c2f01a0
mikan <83708535+piny940@users.noreply.github.com>
2022-08-28 16:45:55 +0900

    TalkListButtonのpropsを作成

--- frontend/components/TalkListButton.tsx
+++ frontend/components/TalkListButton.tsx
@@ -1,11 +1,26 @@
+import { MouseEventHandler } from 'react'
 import styles from '../styles/talk-app.module.scss'
 
-export const TalkListButton: React.FC = () => {
+export interface TalkListButtonProps {
+  handler: MouseEventHandler
+  detail: string
+  title: string
+}
+
+export const TalkListButton: React.FC<TalkListButtonProps> = ({
+  handler,
+  detail,
+  title,
+}) => {
   return (
     <li className={styles.talk_list_button}>
-      <a role="button" className="w-100 h-100 d-block pt-2 ps-4">
-        <h5>トーク名</h5>
-        <div className="detail small text-secondary ms-3">最後のトーク</div>
+      <a
+        role="button"
+        className="w-100 h-100 d-block pt-2 ps-4"
+        onClick={handler}
+      >
+        <h5>{title}</h5>
+        <div className="detail small text-secondary ms-3">{detail}</div>
       </a>
     </li>
   )
