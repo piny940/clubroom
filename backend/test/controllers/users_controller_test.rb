7a78d98211919511a4937e0ebcde5c2ee70ff865
mikan <83708535+piny940@users.noreply.github.com>
2022-08-28 13:18:27 +0900

    users_controllerのテストを修正

--- backend/test/controllers/users_controller_test.rb
+++ backend/test/controllers/users_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
   test '正常に現在自身がログインしているユーザーを取得できる' do
-    user = users('john')
+    user = User.create!(name: 'john', email: 'john@example.com', password: 'password', password_confirmation: 'password')
     sign_in user
     get '/user'
     assert_response :success
