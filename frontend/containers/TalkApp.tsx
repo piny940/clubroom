99d072475430a5cf8b7de9ceae8d2d98f524bd2a
mikan <83708535+piny940@users.noreply.github.com>
2022-08-28 21:29:11 +0900

    TalkAppを作成

--- frontend/containers/TalkApp.tsx
+++ frontend/containers/TalkApp.tsx
@@ -3,13 +3,23 @@ import { LoginRequired } from './LoginRequired'
 import { TalkList } from './TalkList'
 import { Talkroom } from './Talkroom'
 import styles from '../styles/talk-app.module.scss'
+import { useEffect, useState } from 'react'
+import { Talkroom as TalkroomType } from '../types'
+import { useGroupState } from '../contexts/GroupStateProvider'
 
 export const TalkApp: React.FC = () => {
+  const { group } = useGroupState()
+  const [openTalkroom, setOpenTalkroom] = useState<TalkroomType | null>(null)
+
+  useEffect(() => {
+    setOpenTalkroom(null)
+  }, [group])
+
   return (
     <LoginRequired whenNoUser={<AskLogin />}>
       <div className="d-flex" id={styles.app}>
-        <TalkList width="25%" />
-        <Talkroom width="75%" />
+        <TalkList width="25%" setOpenTalkroom={setOpenTalkroom} />
+        <Talkroom width="75%" openTalkroom={openTalkroom} />
       </div>
     </LoginRequired>
   )
