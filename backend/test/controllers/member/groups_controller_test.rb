e89dc690fe7648b6ea6b2316d6d7788af688102c
mikan <83708535+piny940@users.noreply.github.com>
2022-08-28 13:37:37 +0900

    groups_controllerのテストを修正

--- backend/test/controllers/member/groups_controller_test.rb
+++ backend/test/controllers/member/groups_controller_test.rb
@@ -1,14 +1,22 @@
 require 'test_helper'
 
 class Member::GroupsControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = User.create!(name: 'john', email: 'john@example.com', password: 'password', password_confirmation: 'password')
+    @group1 = Group.create!(name: 'group1')
+    @group2 = Group.create!(name: 'group2')
+  end
+
   test '正常にgroupsを取得できる' do
-    user = users('miyuki')
-    sign_in user
+    @group1.members << @user
+
+    sign_in @user
     get '/member/groups'
     assert_response :success
     json = JSON.parse(response.body)
-    assert json['data']['groups']
-    assert_not json['data']['groups'].empty?
+    
+    assert json['data']['groups'].length, 1
+    assert json['data']['groups'][0]["name"], 'group1'
   end
 
   test 'ログインしていない状態では400を返す' do
@@ -19,8 +27,7 @@ class Member::GroupsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'どのグループにも属していない場合は空の配列をgroupsとして返す' do
-    user = users('john')
-    sign_in user
+    sign_in @user
     get '/member/groups'
     assert_response :success
     json = JSON.parse(response.body)
@@ -28,22 +35,20 @@ class Member::GroupsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test '正常に自身の属するグループを作成できる' do
-    user = users('john')
-    sign_in user
-    before_count = user.groups.length
+    sign_in @user
+    before_count = @user.groups.length
     before_groups_count = Group.count
     post '/member/groups', params: { group: { name: 'Test' } }
     assert_response :success
     json = JSON.parse(response.body)
-    user.reload
+    @user.reload
     assert_equal before_groups_count + 1, Group.count
-    assert_equal before_count + 1, user.groups.length
+    assert_equal before_count + 1, @user.groups.length
     assert_equal json['data']['group']['name'], 'Test'
   end
 
   test 'paramsに不備がある場合は400を返す' do
-    user = users('john')
-    sign_in user
+    sign_in @user
     before_count = Group.count
     post '/member/groups', params: { group: { name: '' } }
     assert_response 400
