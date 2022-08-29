407165bccef379198756a6647757eb4563df3628
mikan <83708535+piny940@users.noreply.github.com>
2022-08-29 14:56:40 +0900

    TalkFormを作成

--- frontend/components/TalkForm.tsx
+++ frontend/components/TalkForm.tsx
@@ -1,9 +1,27 @@
+import { FormEventHandler } from 'react'
+import { FieldValues, UseFormRegister } from 'react-hook-form'
 import styles from '../styles/talk-app.module.scss'
 
-export const TalkForm: React.FC = () => {
+export interface TalkFormProps {
+  name: string
+  register: UseFormRegister<FieldValues>
+  requireMessage: string
+  onSubmit: FormEventHandler
+}
+
+export const TalkForm: React.FC<TalkFormProps> = ({
+  name,
+  register,
+  requireMessage,
+  onSubmit,
+}) => {
   return (
-    <form id={styles.talk_form}>
-      <textarea className="form-control" placeholder="トークを入力" />
+    <form id={styles.talk_form} onSubmit={onSubmit}>
+      <textarea
+        className="form-control"
+        placeholder="トークを入力"
+        {...register(name, { required: requireMessage })}
+      />
       <button className="btn">
         <span className="material-icons text-primary">send</span>
       </button>
--- frontend/containers/Talkroom.tsx
+++ frontend/containers/Talkroom.tsx
@@ -1,10 +1,13 @@
 import { useEffect, useState } from 'react'
+import { FieldValues, SubmitHandler, useForm } from 'react-hook-form'
 import { TalkForm } from '../components/TalkForm'
 import { Talks } from '../components/Talks'
+import { useAlertsState } from '../contexts/AlertsStateProvider'
 import { useUserState } from '../contexts/UserStateProvider'
 import styles from '../styles/talk-app.module.scss'
 import { Talk, Talkroom as TalkroomType } from '../types'
-import { fetchTalks } from '../utils/api'
+import { fetchApi, fetchTalks } from '../utils/api'
+import { AlertState } from '../utils/enums'
 
 export interface TalkroomInterface {
   width: string
@@ -17,6 +20,35 @@ export const Talkroom: React.FC<TalkroomInterface> = ({
 }) => {
   const [talks, setTalks] = useState<Talk[]>([])
   const { user } = useUserState()
+  const { setAlerts } = useAlertsState()
+
+  const { register, reset, handleSubmit } = useForm({
+    shouldUseNativeValidation: true,
+  })
+
+  const _submit: SubmitHandler<FieldValues> = async (data) => {
+    if (!openTalkroom) throw new Error('トークルームを選んでください。')
+
+    const response = await fetchApi({
+      url: `/member/groups/${openTalkroom.group_id}/talkrooms/${openTalkroom.id}/talks`,
+      method: 'POST',
+      data: {
+        talk: {
+          content: data.content,
+        },
+      },
+    })
+    const json = await response.json()
+
+    if (response.status >= 400) {
+      setAlerts({
+        content: json.message,
+        state: AlertState.DANGER,
+      })
+    } else {
+      reset()
+    }
+  }
 
   const _updateTalks = async () => {
     if (!openTalkroom) {
@@ -33,8 +65,19 @@ export const Talkroom: React.FC<TalkroomInterface> = ({
 
   return (
     <section style={{ width: width }} className={styles.talk_room}>
-      <Talks talks={talks} userID={user?.id} />
-      <TalkForm />
+      {openTalkroom ? (
+        <>
+          <Talks talks={talks} userID={user?.id} />
+          <TalkForm
+            register={register}
+            name="content"
+            requireMessage="トークを入力してください。"
+            onSubmit={handleSubmit(_submit)}
+          />
+        </>
+      ) : (
+        <></>
+      )}
     </section>
   )
 }
