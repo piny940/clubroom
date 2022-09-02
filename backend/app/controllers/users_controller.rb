a19660399ec2212ffa375492dab66ccfbaa63bd8
mikan <83708535+piny940@users.noreply.github.com>
2022-09-02 22:50:05 +0900

    users_controller#createを作成

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -6,4 +6,31 @@ class UsersController < ApplicationController
       }
     }, status: :ok
   end
+
+  def create
+    user = new User(user_params)
+
+    if user.save
+      sign_in user
+      render json: {
+        data: {
+          user: user
+        },
+        message: 'アカウントが作成されました。'
+      }, status: :ok
+    else
+      render json: {
+        data: {
+          user: nil
+        },
+        message: 'アカウントの作成に失敗しました。'
+      }, status: 400
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :name, :password, :password_confirmation)
+  end
 end
