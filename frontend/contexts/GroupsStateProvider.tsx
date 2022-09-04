7deeb48803907ffd2ca394daebf58bf8104b5ab3
mikan <83708535+piny940@users.noreply.github.com>
2022-09-04 16:29:18 +0900

    GroupsStateProviderを作成

---
+++ frontend/contexts/GroupsStateProvider.tsx
@@ -0,0 +1,39 @@
+import { createContext, ReactNode, useContext, useState } from 'react'
+import { Group } from '../types/index'
+
+interface GroupsStateContextInterface {
+  groups: Group[]
+  setGroups: (groups: Group[]) => void
+}
+
+const defaultContextValue: GroupsStateContextInterface = {
+  groups: [],
+  setGroups: () => undefined,
+}
+
+const GroupsStateContext = createContext(defaultContextValue)
+
+const useGroupsState = () => useContext(GroupsStateContext)
+
+interface GroupsStateProviderProps {
+  children: ReactNode
+}
+
+const GroupsStateProvider: React.FC<GroupsStateProviderProps> = ({
+  children,
+}) => {
+  const [groups, setGroups] = useState<Group[]>([])
+
+  const value: GroupsStateContextInterface = {
+    groups,
+    setGroups,
+  }
+
+  return (
+    <GroupsStateContext.Provider value={value}>
+      {children}
+    </GroupsStateContext.Provider>
+  )
+}
+
+export { useGroupsState, GroupsStateProvider }
