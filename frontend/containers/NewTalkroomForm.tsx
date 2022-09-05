66679588e40d2f74ada7880443d1dbdb9ceb3487
mikan <83708535+piny940@users.noreply.github.com>
2022-09-05 19:48:54 +0900

    Talkroom作成フォームを作成

--- frontend/containers/NewTalkroomForm.tsx
+++ frontend/containers/NewTalkroomForm.tsx
@@ -3,7 +3,7 @@ import { FieldValues, SubmitHandler, useForm } from 'react-hook-form'
 import { FormGroup } from '../components/FormGroup'
 import { ModalFormBox } from '../components/ModalFormBox'
 import { useAlertsState } from '../contexts/AlertsStateProvider'
-import { useGroupsState } from '../contexts/GroupsStateProvider'
+import { useGroupState } from '../contexts/GroupStateProvider'
 import { Message } from '../resources/Messages'
 import { TestID } from '../resources/TestID'
 import { postData } from '../utils/api'
@@ -11,40 +11,48 @@ import { AlertState } from '../utils/enums'
 
 export interface NewTalkroomFormProps {
   targetID: string
+  updateTalkroomList: () => void
 }
 
 export const NewTalkroomForm: React.FC<NewTalkroomFormProps> = ({
   targetID,
+  updateTalkroomList,
 }) => {
   const closeButtonRef = useRef<HTMLButtonElement>(null)
 
-  const { register, handleSubmit } = useForm({
+  const { register, handleSubmit, reset } = useForm({
     shouldUseNativeValidation: true,
   })
 
   const [alert, setFormAlert] = useState<string>('')
   const { setAlerts } = useAlertsState()
-  const { updateGroups } = useGroupsState()
+  const { group } = useGroupState()
 
   const _closeModal = () => {
     closeButtonRef.current?.click()
   }
 
   const _submit: SubmitHandler<FieldValues> = async (data) => {
+    if (!group) {
+      throw new Error('グループが選択されていません。')
+    }
+
     const _onSuccess = (json: any) => {
+      reset()
       _closeModal()
 
       setAlerts({
-        content: 'グループを作成しました。',
+        content: 'トークルームを作成しました。',
         state: AlertState.SUCCESS,
       })
-      updateGroups()
+
+      updateTalkroomList()
     }
 
     void postData({
-      url: '/member/groups',
+      url: `/member/groups/${group.id}/talkrooms`,
       data: data,
-      scope: 'group',
+      scope: 'talkroom',
       onFail: (json: any) => setFormAlert(json.message),
       onSuccess: _onSuccess,
     })
@@ -55,25 +63,18 @@ export const NewTalkroomForm: React.FC<NewTalkroomFormProps> = ({
       title="新規トークルーム作成"
       alert={alert}
       submitButtonText="送信"
-      submitTestID={TestID.GROUP_FORM_SUBMIT}
+      submitTestID={TestID.TALKROOM_FORM_SUBMIT}
       targetID={targetID}
       onSubmit={handleSubmit(_submit)}
       closeButtonRef={closeButtonRef}
     >
       <FormGroup
         register={register}
-        label="グループ名"
+        label="トークルーム名"
         type="text"
         name="name"
         required={Message.INPUT_REQUIRED}
-        testID={TestID.GROUP_FORM_NAME}
-      />
-      <FormGroup
-        register={register}
-        label="学校"
-        type="text"
-        name="school"
-        testID={TestID.GROUP_FORM_SCHOOL}
+        testID={TestID.TALKROOM_FORM_NAME}
       />
     </ModalFormBox>
   )
