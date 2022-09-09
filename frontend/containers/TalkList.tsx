e96650b499b47e02c22fb0eaf991c0c836c68bbd
mikan <83708535+piny940@users.noreply.github.com>
2022-09-09 17:16:21 +0900

    TalkListとTalkAppのテストを修正

--- frontend/containers/TalkList.tsx
+++ frontend/containers/TalkList.tsx
@@ -7,7 +7,7 @@ import { NewTalkroomForm } from './NewTalkroomForm'
 import { TestID } from '../resources/TestID'
 import { useGroupState } from '../contexts/GroupStateProvider'
 
-export interface TalkListInterface {
+export interface TalkListProps {
   width: string
   setOpenTalkroom: (talkroom: Talkroom) => void
   openTalkroom: Talkroom | undefined
@@ -15,7 +15,7 @@ export interface TalkListInterface {
   updateTalkroomList: () => void
 }
 
-export const TalkList: React.FC<TalkListInterface> = ({
+export const TalkList: React.FC<TalkListProps> = ({
   width,
   setOpenTalkroom,
   openTalkroom,
@@ -30,39 +30,41 @@ export const TalkList: React.FC<TalkListInterface> = ({
       className={toClass('h-100', styles.talk_list)}
     >
       {group ? (
-        <div
-          className={toClass(
-            'ps-4 pe-1 py-2 d-flex flex-column',
-            styles.action_buttons
-          )}
-        >
-          <TalkListActionButton
-            handler={() => undefined}
-            iconName="add_circle"
-            label="新規トークルーム作成"
-            iconColor="#00d77b" // Green
-            modalID="new-talkroom"
-            testID={TestID.NEW_TALKROOM_BUTTON}
+        <>
+          <div
+            className={toClass(
+              'ps-4 pe-1 py-2 d-flex flex-column',
+              styles.action_buttons
+            )}
+          >
+            <TalkListActionButton
+              handler={() => undefined}
+              iconName="add_circle"
+              label="新規トークルーム作成"
+              iconColor="#00d77b" // Green
+              modalID="new-talkroom"
+              testID={TestID.NEW_TALKROOM_BUTTON}
+            />
+          </div>
+          <ul className="p-0">
+            {talkrooms.map((talkroom) => (
+              <TalkListButton
+                key={talkroom.id}
+                title={talkroom.name}
+                handler={() => setOpenTalkroom(talkroom)}
+                open={talkroom.id === openTalkroom?.id}
+              />
+            ))}
+          </ul>
+          <NewTalkroomForm
+            targetID="new-talkroom"
+            updateTalkroomList={updateTalkroomList}
+            setOpenTalkroom={setOpenTalkroom}
           />
-        </div>
+        </>
       ) : (
         <></>
       )}
-      <ul className="p-0">
-        {talkrooms.map((talkroom) => (
-          <TalkListButton
-            key={talkroom.id}
-            title={talkroom.name}
-            handler={() => setOpenTalkroom(talkroom)}
-            open={talkroom.id === openTalkroom?.id}
-          />
-        ))}
-      </ul>
-      <NewTalkroomForm
-        targetID="new-talkroom"
-        updateTalkroomList={updateTalkroomList}
-        setOpenTalkroom={setOpenTalkroom}
-      />
     </section>
   )
 }
