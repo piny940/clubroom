c8cacaf1542fd940ae0f6666d0d7843bc050c5c9
mikan <83708535+piny940@users.noreply.github.com>
2022-09-11 00:39:55 +0900

    entry_tokenを用いてTalkroomに参加できるようにする

--- backend/app/controllers/member/groups/talk_entries_controller.rb
+++ backend/app/controllers/member/groups/talk_entries_controller.rb
@@ -1,6 +1,23 @@
-class Member::Groups:::TalkEntriesController < Member::Groups::Base
+class Member::Groups::TalkEntriesController < Member::Groups::Base
   def create
     talkroom = Talkroom.find(params[:talkroom_id])
-    
+    if talkroom.entry_token == params[:entry_token]
+      talkroom.members << current_user
+      talkroom.set_token
+      talkroom.save!
+      talk_entry = current_user.talk_entries.find_by(talkroom_id: talkroom.id)
+      
+      render json: {
+        message: 'トークルームに参加しました。',
+        data: {
+          talkroom: talkroom,
+          talk_entry: talk_entry
+        }
+      }, status: :ok
+    else
+      render json: {
+        message: 'トークンが違います。'
+      }, status: :bad_request
+    end
   end
 end
---
+++ backend/test/controllers/member/groups/talk_entries_controller_test.rb
@@ -0,0 +1,40 @@
+require 'test_helper'
+
+class Member::Groups::TalkEntriesControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = User.create!(name: 'alice', email: 'alice@example.com', password: 'password', password_confirmation: 'password')
+    @group1 = Group.create!(name: 'group1')
+    @group1.members << @user
+    @room1 = @group1.talkrooms.create!(name: 'room1')
+    sign_in @user
+  end
+
+  test '正常に正しいentry_tokenを用いてトークルームに参加できる' do
+    before_count = @room1.members.length
+    before_token = @room1.entry_token
+    post "/member/groups/#{@room1.group.id}/talkrooms/#{@room1.id}/talk_entry", params: { entry_token: @room1.entry_token }
+
+    assert_response :success
+    json = JSON.parse(response.body)
+    @room1.reload
+
+    assert_equal before_count + 1, @room1.members.length
+    assert_equal 'room1', json['data']['talkroom']['name']
+    assert_equal 'member', json['data']['talk_entry']['role']
+    assert_not_equal before_token, @room1.entry_token
+  end
+
+  test 'entry_tokenが間違っている場合トークルームに参加できない' do
+    before_count = @room1.members.length
+    before_token = @room1.entry_token
+    post "/member/groups/#{@room1.group.id}/talkrooms/#{@room1.id}/talk_entry", params: { entry_token: 'wrong_token' }
+
+    assert_response 400
+    json = JSON.parse(response.body)
+    @room1.reload
+
+    assert_equal before_count, @room1.members.length
+    assert_equal before_token, @room1.entry_token
+    assert_equal 'トークンが違います。', json['message']
+  end
+end
