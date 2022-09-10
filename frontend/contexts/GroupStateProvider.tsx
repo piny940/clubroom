605f99f667d1bad90477248b4a52053e966af6f4
mikan <83708535+piny940@users.noreply.github.com>
2022-09-10 17:35:59 +0900

    userを変更したらgroupはundefinedになるよう修正

--- frontend/contexts/GroupStateProvider.tsx
+++ frontend/contexts/GroupStateProvider.tsx
@@ -1,5 +1,12 @@
-import { createContext, ReactNode, useContext, useState } from 'react'
+import {
+  createContext,
+  ReactNode,
+  useContext,
+  useEffect,
+  useState,
+} from 'react'
 import { Group } from '../types'
+import { useUserState } from './UserStateProvider'
 
 interface GroupStateContextInterface {
   group: Group | undefined
@@ -23,6 +30,11 @@ const GroupStateProvider: React.FC<GroupStateProviderProps> = ({
   children,
 }) => {
   const [group, setGroup] = useState<Group | undefined>(undefined)
+  const { user } = useUserState()
+
+  useEffect(() => {
+    setGroup(undefined)
+  }, [user])
 
   const value: GroupStateContextInterface = {
     group,
