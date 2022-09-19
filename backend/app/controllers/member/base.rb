10f386cba575b3d0c7b630b14c71fe2d7f5dc300
mikan <83708535+piny940@users.noreply.github.com>
2022-09-19 20:54:27 +0900

    不要なコードを削除

--- backend/app/controllers/member/base.rb
+++ backend/app/controllers/member/base.rb
@@ -1,13 +1,3 @@
 class Member::Base < ApplicationController
   before_action :authenticate_user!
-
-  private
-
-  def authenticate_user!
-    if current_user.nil?
-      render json: {
-        message: 'ログインしてください。'
-      }, status: :bad_request
-    end
-  end
 end
