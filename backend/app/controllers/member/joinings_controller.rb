e94f7b4cc12d6a712586852464da2c9322ea69e0
mikan <83708535+piny940@users.noreply.github.com>
2022-09-19 21:52:23 +0900

    JoiningsController#showのテストを作成

---
+++ backend/app/controllers/member/joinings_controller.rb
@@ -0,0 +1,9 @@
+class Member::JoiningsController < Member::Base
+  def show
+
+  end
+  
+  def create
+
+  end
+end
---
+++ backend/test/controllers/member/joinings_controller_test.rb
@@ -0,0 +1,30 @@
+require 'test_helper'
+
+class Member::JoiningsControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = User.create!(name: 'john', email: 'john@example.com', password: 'password', password_confirmation: 'password')
+    @group1 = Group.create!(name: 'group1')
+    @group2 = Group.create!(name: 'group2')
+
+    sign_in @user
+    @group1.members << @user
+  end
+
+  test 'Joiningを正常に取得できる' do
+    get "/member/groups/#{@group1.id}/joining"
+
+    assert_response :success
+    json = JSON.parse(response.body)
+
+    assert_equal @user.joinings.find_by(group_id: @group1.id).id, json['data']['joining']['id']
+  end
+
+  test '自身が属していないグループに対してはjoiningをnilとして返す' do
+    get "/member/groups/#{@group2.id}/joining"
+
+    assert_response 400
+    json = JSON.parse(response.body)
+
+    assert_nil json['data']['joining']
+  end
+end
