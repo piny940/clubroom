47afec305e141f609c9d592d708520ede1be51b9
mikan <83708535+piny940@users.noreply.github.com>
2022-09-22 13:47:55 +0900

    Tooltipを作成

--- frontend/components/Common/Tooltip.tsx
+++ frontend/components/Common/Tooltip.tsx
@@ -1,19 +1,44 @@
-import { useRef } from 'react'
+import { ReactNode, useEffect, useRef, useState } from 'react'
 import style from '../../styles/common.module.scss'
 
 export interface TooltipProps {
   text: string
+  children: ReactNode
 }
 
-export const Tooltip: React.FC<TooltipProps> = ({ text }) => {
+export const Tooltip: React.FC<TooltipProps> = ({ text, children }) => {
   const tooltipRef = useRef<HTMLDivElement>(null)
+
+  const [isShown, setIsShown] = useState(false)
+
+  useEffect(() => {
+    if (!tooltipRef.current) return
+
+    const targetEl = tooltipRef.current.nextElementSibling
+
+    if (!targetEl) return
+
+    targetEl.addEventListener('mouseover', () => {
+      setIsShown(true)
+    })
+    targetEl.addEventListener('mouseleave', () => {
+      setIsShown(false)
+    })
+  })
 
   return (
     <>
-      <div ref={tooltipRef} className={style.tooltip} role="tooltip">
-        {text}
+      <div ref={tooltipRef} role="tooltip">
+        {isShown ? (
+          <>
+            <div className={style.tooltip}>{text}</div>
+            <span className={style.tooltip_arrow}></span>
+          </>
+        ) : (
+          <></>
+        )}
       </div>
-      <span className={style.tooltip_arrow}></span>
+      {children}
     </>
   )
 }
