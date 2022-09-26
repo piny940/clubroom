ce2a489a8ad3a9abd90c45a3767c49bd8016a156
mikan <83708535+piny940@users.noreply.github.com>
2022-09-27 00:46:13 +0800

    PageListButtonにonClickを追加

--- frontend/components/GroupMenu/PageListButton.tsx
+++ frontend/components/GroupMenu/PageListButton.tsx
@@ -1,12 +1,14 @@
 import { toClass } from '../../utils/helpers'
 import styles from '../../styles/group-menu.module.scss'
 import Link from 'next/link'
+import { MouseEventHandler } from 'react'
 
 export interface PageListButtonProps {
   open: Boolean
   testID?: string
   title: string
   link: string
+  onClick?: MouseEventHandler
 }
 
 export const PageListButton: React.FC<PageListButtonProps> = ({
@@ -14,20 +16,15 @@ export const PageListButton: React.FC<PageListButtonProps> = ({
   testID,
   title,
   link,
+  onClick = () => undefined,
 }) => {
   return (
-    <li
-      className={toClass(
-        'position-relative',
-        open ? styles.open : '',
-        styles.page_list_button
-      )}
-    >
+    <li className={toClass(open ? styles.open : '', styles.page_list_button)}>
       <Link href={link}>
         <a
           role="button"
           className="h-100 d-block pt-2 px-4"
-          onClick={() => undefined}
+          onClick={onClick}
           data-testid={testID}
         >
           <h5>{title}</h5>
