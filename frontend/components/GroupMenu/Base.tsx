99d3d20f11393e29cb0e96b89ab0e257ed6aa117
mikan <83708535+piny940@users.noreply.github.com>
2022-09-27 00:51:43 +0800

    GroupMenu画面でgroupが選択されていないときに表示する画面を作成

--- frontend/components/GroupMenu/Base.tsx
+++ frontend/components/GroupMenu/Base.tsx
@@ -1,18 +1,24 @@
 import { ReactNode } from 'react'
 import { PageList } from './PageList'
 import styles from '../../styles/group-menu.module.scss'
+import { useUserInfo } from '../../contexts/UserInfoProvider'
+import { NoGroupSelected } from './NoGroupSelected'
 
 export interface BaseProps {
   children: ReactNode
 }
 
 export const Base: React.FC<BaseProps> = ({ children }) => {
-  return (
+  const { group } = useUserInfo()
+
+  return group ? (
     <div className="d-flex w-100" id={styles.app}>
       <PageList width="20%" />
       <div className="d-flex bg-white" style={{ width: '80%' }}>
         {children}
       </div>
     </div>
+  ) : (
+    <NoGroupSelected />
   )
 }
--- frontend/containers/GroupMenu/MembersApp.tsx
+++ frontend/containers/GroupMenu/MembersApp.tsx
@@ -1,3 +1,11 @@
+import { useUserInfo } from '../../contexts/UserInfoProvider'
+
 export const MembersApp: React.FC = () => {
-  return <>hoge</>
+  const { group } = useUserInfo()
+
+  return (
+    <div className="container">
+      {group ? group.name : 'Group is not selected'}
+    </div>
+  )
 }
