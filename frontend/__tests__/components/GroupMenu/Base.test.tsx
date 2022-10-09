0c702716babf4240e4fb66a1590862ee1b0216e5
mikan <83708535+piny940@users.noreply.github.com>
2022-10-09 22:18:44 +0900

    GroupMenu/Baseのテストを作成

---
+++ frontend/__tests__/components/GroupMenu/Base.test.tsx
@@ -0,0 +1,26 @@
+import { render, waitFor } from '@testing-library/react'
+import { ReactNode } from 'react'
+import { Base } from '../../../components/GroupMenu/Base'
+import { LoginRequired } from '../../../containers/LoginRequired'
+import { Test } from '../../testHelpers/Test'
+import { expect } from '@jest/globals'
+
+jest.mock('../../../containers/LoginRequired')
+const mockedLoginRequired = jest.mocked(LoginRequired)
+mockedLoginRequired.mockImplementation(
+  ({ children }: { children: ReactNode }) => <>{children}</>
+)
+
+describe('<Base />', () => {
+  it('正常に動作する', async () => {
+    const component = render(
+      <Base>
+        <Test />
+      </Base>
+    )
+
+    await waitFor(() => {
+      expect(component).toBeTruthy()
+    })
+  })
+})
