52f779b281c71e8272f947b6104ffdd7eb571e40
piny940 <an.s.shoma@gmail.com>
2022-11-02 17:26:15 +0900

    Login画面の遷移先のページを指定できるようにする

--- frontend/containers/LoginForm.tsx
+++ frontend/containers/LoginForm.tsx
@@ -3,7 +3,7 @@ import { InputBox } from '../components/Common/InputBox'
 import { Message } from '../resources/Messages'
 import { AlertState } from '../resources/enums'
 import styles from '../styles/accounts.module.scss'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useMovePage } from '../utils/hooks'
 import { postData } from '../utils/api'
 import { TestID } from '../resources/TestID'
@@ -11,21 +11,30 @@ import Link from 'next/link'
 import { FormBox } from '../components/Common/FormBox'
 import { useUserInfo } from '../contexts/UserInfoProvider'
 import { CheckBox } from '../components/Common/CheckBox'
+import { useRouter } from 'next/router'
 
 export const LoginForm: React.FC = () => {
   const { register, handleSubmit } = useForm({
     shouldUseNativeValidation: true,
   })
+  const router = useRouter()
 
   const [alert, setAlert] = useState<string>('')
+  const [nextPage, setNextPage] = useState('/')
   const { updateUser } = useUserInfo()
 
   const movePage = useMovePage()
 
+  useEffect(() => {
+    if (router.isReady && typeof router.query.next === 'string') {
+      setNextPage(router.query.next)
+    }
+  }, [router.isReady])
+
   const _submit: SubmitHandler<FieldValues> = async (data) => {
     const _onSuccess = (json: any) => {
       void updateUser()
-      void movePage('/', {
+      void movePage(nextPage, {
         content: json.message,
         state: AlertState.SUCCESS,
       })
