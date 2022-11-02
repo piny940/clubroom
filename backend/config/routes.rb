cd21663bc763e0f1af44d89b023b3669efd94468
piny940 <an.s.shoma@gmail.com>
2022-11-02 23:26:08 +0900

    グループ/トークルームから抜けるアクションをroutes.rbに追加

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -15,11 +15,11 @@ Rails.application.routes.draw do
   resource :user, only: %i[show create update destroy]
   namespace :member do
     resources :groups, only: %i[index create update destroy show] do
-      resource :joining, only: %i[create show]
+      resource :joining, only: %i[create show destroy]
       scope module: :groups do
         resources :members, only: %i[index]
         resources :talkrooms, only: %i[index create show destroy update] do
-          resource :talk_entry, only: %i[create show]
+          resource :talk_entry, only: %i[create show destroy]
           scope module: :talkrooms do
             resources :talks, only: %i[index create]
             resources :members, only: %i[index]
