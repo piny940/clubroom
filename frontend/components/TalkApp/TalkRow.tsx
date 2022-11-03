6a14008780f03c14d94b98a6d4a1a67d81d6a773
piny940 <an.s.shoma@gmail.com>
2022-11-03 12:46:00 +0900

    トークに送り主のアイコンと名前を表示する

--- frontend/components/TalkApp/TalkRow.tsx
+++ frontend/components/TalkApp/TalkRow.tsx
@@ -2,7 +2,9 @@ import { useUserInfo } from '../../contexts/UserInfoProvider'
 import { TestID } from '../../resources/TestID'
 import { Talk as TalkType } from '../../resources/types'
 import { toClass } from '../../utils/helpers'
+import { AccountIcon } from '../Common/AccountIcon'
 import { Talk } from './Talk'
+import styles from '../../styles/talk-app.module.scss'
 
 export interface TalkRowProps {
   talk: TalkType
@@ -14,11 +16,20 @@ export const TalkRow: React.FC<TalkRowProps> = ({ talk }) => {
   return (
     <li
       className={toClass(
-        'w-100 my-2 px-4 d-flex',
-        `justify-content-${talk.from_user_id === user?.id ? 'end' : 'start'}`
+        'w-100 my-2 px-4 d-flex align-items-end',
+        `flex-row${talk.from_user_id === user?.id ? '-reverse' : ''}`
       )}
       data-testid={TestID.TALK_ROW}
     >
+      <div
+        className={toClass(
+          `m${talk.from_user_id === user?.id ? 's' : 'e'}-1`,
+          'd-flex flex-column align-items-center'
+        )}
+      >
+        <AccountIcon theme="light" size={27} src={talk.from_user.global_icon} />
+        <div className={styles.from_user_name}>{talk.from_user.name}</div>
+      </div>
       <Talk talk={talk} />
     </li>
   )
