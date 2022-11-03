a613f9e2493a5435ca81ae124818a06aec484778
piny940 <an.s.shoma@gmail.com>
2022-11-03 12:18:09 +0900

    Talkコンポーネント周りをリファクタ

--- frontend/components/TalkApp/TalkRow.tsx
+++ frontend/components/TalkApp/TalkRow.tsx
@@ -1,22 +1,25 @@
+import { useUserInfo } from '../../contexts/UserInfoProvider'
 import { TestID } from '../../resources/TestID'
+import { Talk as TalkType } from '../../resources/types'
 import { toClass } from '../../utils/helpers'
 import { Talk } from './Talk'
 
 export interface TalkRowProps {
-  content: string
-  sentFrom: 'myself' | 'others'
+  talk: TalkType
 }
 
-export const TalkRow: React.FC<TalkRowProps> = ({ content, sentFrom }) => {
+export const TalkRow: React.FC<TalkRowProps> = ({ talk }) => {
+  const { user } = useUserInfo()
+
   return (
     <li
       className={toClass(
         'w-100 my-2 px-4 d-flex',
-        `justify-content-${sentFrom === 'myself' ? 'end' : 'start'}`
+        `justify-content-${talk.from_user_id === user?.id ? 'end' : 'start'}`
       )}
       data-testid={TestID.TALK_ROW}
     >
-      <Talk content={content} sentFrom={sentFrom} />
+      <Talk talk={talk} />
     </li>
   )
 }
