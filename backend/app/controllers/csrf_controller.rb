b10f0e4f7c0d551741422509af685dfe06328709
piny940 <an.s.shoma@gmail.com>
2024-02-01 14:24:40 +0900

    不要なコードを削除

--- backend/app/controllers/csrf_controller.rb
+++ backend/app/controllers/csrf_controller.rb
@@ -1,7 +1,5 @@
 class CsrfController < ApplicationController
   def show
-    print request.headers.to_h.to_s.gsub(", ", "\n")
-
     render json: {
       data: {
         token: form_authenticity_token
