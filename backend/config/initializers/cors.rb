512a8f18488712e5a215f1724ae3e20e1fd25a32
piny940 <83708535+piny940@users.noreply.github.com>
2024-08-12 11:45:21 +0900

    corsに新しいURLの設定を追加

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -2,7 +2,11 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins [
       'http://localhost:3001',
-      'https://clubrooms.herokuapp.com'
+      'https://clubrooms.herokuapp.com',
+      'https://clubroom.piny940.com',
+      'https://stg-clubroom.piny940.com',
+      'https://clubroom-backend.piny940.com',
+      'https://stg-clubroom-backend.piny940.com'
     ]
     resource '*',
              method: %i[get post put patch delete],
