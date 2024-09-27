a45354984d90936f12e8703b2321761d55741221
piny940 <83708535+piny940@users.noreply.github.com>
2024-09-27 12:17:17 +0900

    stg-clubroom.piny940.comをallow

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -3,7 +3,8 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     origins [
       'http://localhost:3001',
       'https://clubrooms.herokuapp.com',
-      'https://clubroom.piny940.com'
+      'https://clubroom.piny940.com',
+      'https://stg-clubroom.piny940.com'
     ]
     resource '*',
              method: %i[get post put patch delete],
